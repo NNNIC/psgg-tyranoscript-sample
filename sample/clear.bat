ae39852a592c5eeda1c336d6af730ac56b9bfe96
nn4343 <gea01441@nifty.com>
2019-05-11 15:01:10 +0900

    no message

--- sample/clear.bat
+++ sample/clear.bat
@@ -2,4 +2,18 @@ cd /d %~dp0
 
 del /q doc\*.*
 del /q src\*.*
+
+@echo off
+echo ...
+:_loop
+echo : copy reference  (リファレンスをコピーしますか)　? 
+set /p a="Y or N :"
+if "%a%"=="Y" goto :_copyref
+if "%a%"=="N" goto :_end
+goto :_loop
+
+:_copyref
+copy ref\doc\*.* doc\*.*
+copy ref\src\*.* src\*.*
+:_end
 pause
